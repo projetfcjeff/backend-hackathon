b711a26773160fba2352b21c0ffeeb0128693631
jfbonglet <jean.francois.bonglet@gmail.com>
2024-01-30 16:50:27 +0100

    RAS

--- routes/trips.js
+++ routes/trips.js
@@ -6,7 +6,8 @@ const Trip = require("../models/trips");
 /* Homepage - GET - Retourne la liste des voyages correspondants aux critères de saisie. 
 3 critères passés en BODY dans la requête : departure / arrival / date */
 //Retourne erreur si rien ne correspond en affichant le logo de la loupe sur le front
-//!Attention à la gestion des dates
+//!Attention à la gestion des dates - format ISO8601
+
 router.get('/', function(req, res) {
     const filter = {
         departure: { $regex: new RegExp(req.body.departure, 'i') },
