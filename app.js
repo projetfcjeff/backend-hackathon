3bbe4447739b605802e7bd384c4ffb5eac453c74
jfbonglet <jean.francois.bonglet@gmail.com>
2024-01-30 15:10:04 +0100

    export route trips dans app.js

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ require("./models/connection");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+var tripsRouter = require("./routes/trips");
 
 var app = express();
 
@@ -23,5 +24,6 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/trips", tripsRouter);
 
 module.exports = app;
