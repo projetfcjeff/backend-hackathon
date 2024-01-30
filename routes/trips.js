1ad9f6274aec488c477849d3635e1c29b706ee92
jfbonglet <jean.francois.bonglet@gmail.com>
2024-01-30 16:14:33 +0100

    RouteGetTrip v1

--- routes/trips.js
+++ routes/trips.js
@@ -1,13 +1,33 @@
 var express = require('express');
 var router = express.Router();
 
+const Trip = require("../models/trips");
+
 /* Homepage - GET - Retourne la liste des voyages correspondants aux critères de saisie. 
 3 critères passés en BODY dans la requête : departure / arrival / date */
 //Retourne erreur si rien ne correspond en affichant le logo de la loupe sur le front
 //!Attention à la gestion des dates
 router.get('/', function(req, res) {
-  
-    
+    const filter = {
+        departure: { $regex: new RegExp(req.body.cityName, 'i') },
+        arrival: { $regex: new RegExp(req.body.arrival, 'i') },
+        date: req.body.date,
+    }
+    Trip.find(filter)
+    .then(result => 
+        {
+            if (result.length > 0) 
+                {
+                    res.json(
+                        {
+                            status : 200, 
+                            result: result,
+                        })
+                }
+            else res.json({status: 400, result: false, error: "Trip not found" })
+        }
+        )
+
 });
 
 module.exports = router;
